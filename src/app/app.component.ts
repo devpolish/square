37077b01ebe2cd353aa41ea3a9bd3a4dc0d4adfa
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2017-10-13 05:14:45 -0500

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  titles:any = [
+    { title: 'Luxury places', image: 'https://media.timeout.com/images/100894675/image.jpg'},
+    { title: 'Good music', image: 'https://img1.10bestmedia.com/Images/Photos/328870/p-1496541-783832721664354-1485815104510667131-o_54_990x660.jpg'},
+    { title: 'Warm people', image: 'https://img1.10bestmedia.com/Images/Photos/343513/Insanity-5_54_990x660.jpg'},
+  ];
 }
